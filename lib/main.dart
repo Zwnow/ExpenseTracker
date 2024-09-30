4abeaa05489b030c9bac2264ef1c46d5bdad8321
Sven O <svenotimm@gmail.com>
2024-09-30 21:15:07 +0200

    replaced landing page widget with futurebuilder

--- lib/main.dart
+++ lib/main.dart
@@ -29,34 +29,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Dog {
-  final int id;
-  final String name;
-  final int age;
-
-  const Dog({
-    required this.id,
-    required this.name,
-    required this.age,
-  });
-
-  Map<String, Object?> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'age': age,
-    };
-  }
-
-  @override
-  String toString() {
-    return 'Dog{id: $id, name: $name, age: $age}';
-  }
-
-}
-
 class MyAppState extends ChangeNotifier {
-    final DatabaseHelper dbhelper = DatabaseHelper();
+  final DatabaseHelper dbhelper = DatabaseHelper();
 }
 
 class MyHomePage extends StatefulWidget {
@@ -83,35 +57,38 @@ class _MyHomePageState extends State<MyHomePage> {
         body: Row(
           children: [
             Expanded(
-              child: Container(
-                color: Colors.red,
-                child: page
-              ),
+              child: Container(color: const Color.fromARGB(255, 255, 255, 255), child: page),
             )
+
           ],
         ),
         bottomNavigationBar: BottomAppBar(
-          color: Colors.blue,
-          child: IconTheme(
-            data: IconThemeData(
-              color: Theme.of(context).colorScheme.onPrimary,
-          ), child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(onPressed: () {
-                setState(() {
-                  selectedIndex = 0;
-                });
-              }, icon:const Icon(Icons.menu), tooltip: 'Menu',),
-              IconButton(onPressed: () {
-                setState(() {
-                  selectedIndex = 1;
-                });
-              }, icon:const Icon(Icons.ac_unit)),
-            ],
-          ))
-        ),
-        
+            color: const Color.fromARGB(255, 29, 29, 29),
+            child: IconTheme(
+                data: IconThemeData(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          selectedIndex = 0;
+                        });
+                      },
+                      icon: const Icon(Icons.account_box),
+                      tooltip: 'Menu',
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            selectedIndex = 1;
+                          });
+                        },
+                        icon: const Icon(Icons.menu)),
+                  ],
+                ))),
       );
     });
   }
@@ -124,6 +101,19 @@ class LandingPage extends StatefulWidget {
 
 class _LandingPageState extends State<LandingPage> {
   List<Account> _data = [];
+  Future<void>? _futureData;
+
+  String displayMoney(double amount) {
+    final text = amount.toString();
+    final euro = text.split('.')[0];
+    final cents = text.split('.')[1];
+
+    if (int.parse(cents) <= 9) {
+      return "$euro.0$cents€";
+    } else {
+      return "$euro.$cents€";
+    }
+  }
 
   Future<void> fetchData() async {
     final DatabaseHelper dbhelper = DatabaseHelper();
@@ -132,6 +122,18 @@ class _LandingPageState extends State<LandingPage> {
     setState(() {
       _data = accounts;
     });
+  }
+
+  void _setData() {
+    setState(() {
+      _futureData = fetchData();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _setData();
   }
 
   final _formKey = GlobalKey<FormState>();
@@ -143,84 +145,108 @@ class _LandingPageState extends State<LandingPage> {
     super.dispose();
   }
 
-@override
-Widget build(BuildContext context) {
-  return ListView(
-    padding: EdgeInsets.symmetric(horizontal: 20),
-    children: [
-      SizedBox(height: 40),
-      Center(child: Text('Landing Page', style: TextStyle(fontSize: 24))),
-      if (_data.isEmpty) ...[
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      children: [
+        SizedBox(height: 40),
+        Center(child: Text('Startseite', style: TextStyle(fontSize: 24))),
         SizedBox(height: 30),
-        Text(
-          "Willkommen, so wie es aussieht hast du noch keinen Account.",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 16),
-        ),
-        SizedBox(height: 30),
-        Text(
-          "Bitte gib dem Account hier einen Namen:",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 16),
-        ),
-        SizedBox(height: 16),
-        Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _nameEditingController,
-                decoration: InputDecoration(
-                  labelText: "Name",
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Bitte geben Sie einen gültigen Namen ein.";
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    String name = _nameEditingController.text;
-                    Account account = Account(id: 0, name: name, total: 0.00);
+        FutureBuilder(
+            future: _futureData,
+            builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return ListBody(
+                  children: [Center(child: CircularProgressIndicator())],
+                );
+              } else if (snapshot.connectionState == ConnectionState.done &&
+                  _data.isEmpty) {
+                return ListBody(
+                  children: [
+                    Text(
+                      "Willkommen, so wie es aussieht hast du noch keinen Account.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 30),
+                    Text(
+                      "Bitte gib dem Account hier einen Namen:",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 16),
+                    Form(
+                        key: _formKey,
+                        child: Column(children: [
+                          TextFormField(
+                            controller: _nameEditingController,
+                            decoration: InputDecoration(
+                              labelText: "Name",
+                              border: OutlineInputBorder(),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Bitte geben Sie einen gültigen Namen ein.";
+                              }
+                              if (value.length >= 15) {
+                                return "Es können maximal 15 Zeichen genutzt werden.";
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(height: 16),
+                          ElevatedButton(
+                            onPressed: () async {
+                              if (_formKey.currentState!.validate()) {
+                                String name = _nameEditingController.text;
+                                Account account =
+                                    Account(id: 0, name: name, total: 0.00);
 
-                    DatabaseHelper dh = DatabaseHelper();
-                    await dh.insertAccount(account);
+                                DatabaseHelper dh = DatabaseHelper();
+                                await dh.insertAccount(account);
 
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Der Account wurde erstellt.')),
-                    );
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      content:
+                                          Text('Der Account wurde erstellt.')),
+                                );
 
-                    // Reload the data and update the UI
-                    setState(() {
-                      fetchData();
-                    });
-                  }
-                },
-                child: Text("Bestätigen"),
-              ),
-            ],
-          ),
-        ),
-      ] else ...[
-        ListBody(
-          children: [
-            Text("Kontoliste:", style: TextStyle(fontSize: 16)),
-            SizedBox(height: 10),
-            for (var account in _data) // Assuming _data contains the list of accounts
-              ListTile(
-                title: Text(account.name), // Display account name
-              ),
-          ],
-        ),
+                                // Reload the data and update the UI
+                                  _setData();
+                              }
+                            },
+                            child: Text("Bestätigen"),
+                          ),
+                        ])),
+                  ],
+                );
+              } else if (snapshot.connectionState == ConnectionState.done &&
+                  _data.isNotEmpty) {
+                return ListBody(
+                  children: [
+                    Center(
+                      child: Text(_data.first.name,
+                      style: TextStyle(fontSize: 32)),),
+                    SizedBox(height: 30),
+                    Center(child: Text("Ausgaben", style: TextStyle(fontSize: 24)),),
+                    SizedBox(height: 10),
+                    Center(
+                      child: Text(
+                        displayMoney(_data.first.total),
+                        style: TextStyle(fontSize: 32),
+                      ),
+                    ),
+                  ],
+                );
+              } else {
+                return Center(
+                    child: Text('Es gab einen Fehler beim laden der Daten.'));
+              }
+            }),
       ],
-    ],
-  );
-}
+    );
+  }
 
 }
 class OtherPage extends StatelessWidget {
